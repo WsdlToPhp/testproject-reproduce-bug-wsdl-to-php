ccf9c92e142e556f887597fe81abcdfc312018c2
Lenz Weber <mail@lenzw.de>
2015-08-20 14:21:16 +0200

    test project intial commit

---
+++ test-WCFService/IService1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace test_WCFService
+{
+    [ServiceContract(Namespace = "http://test/WCFService")]
+    public interface IService1
+    {
+
+        [OperationContract]
+        int order(order order);
+
+        [OperationContract]
+        int offer(offer offer);
+    }
+
+    [DataContract]
+    public class order
+    {
+        [DataMember]
+        public string orderClassMember;
+    }
+
+    [DataContract]
+    public class offer : order
+    {
+        [DataMember]
+        public string offerClassMember;
+    }
+}
